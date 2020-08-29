956e894deb65f4f120293f4b0fe0d4e6d6509401
Robin Hanjun Kwon <khj02021@gmail.com>
2020-08-29 17:44:31 +0900

    [PG14][UI01] Implemented lifebar UI and player life system
    [PG14] As a player I want it so that when I fall into a pit I am teleported back to the last spot I was so that I don't fall forever [8] - 1.5 hour
    [UI01] As a player, I want to have a life bar made up of hearts so that there is a fail state in the game [5] - 1.5 hour
      - Now player has three lives
      - Three lives are shown left top of the game as hearts
      - When player falls into the pit, player respawn to last spot
      - When player falls into the pit, the hearts indicate player that player got damaged
      - GameManager script has been created to implement this system

---
+++ Assets/Scripts/Player/Respawn.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter playerCharacter;
+    [SerializeField] private GameObject player;
+
+    private Vector3 respawnPosition;
+    private Vector3 pitCollisionPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerCharacter.IsGrounded()) {
+            respawnPosition = player.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+            pitCollisionPosition = player.transform.position;
+
+            if(pitCollisionPosition.x > respawnPosition.x) {
+                respawnPosition = new Vector3(respawnPosition.x - 1f, respawnPosition.y);
+                collision.transform.position = respawnPosition;
+                GameManager.instance.damage();
+            }
+            else {
+                respawnPosition = new Vector3(respawnPosition.x + 1f, respawnPosition.y);
+                collision.transform.position = respawnPosition;
+                GameManager.instance.damage();
+            }
+        }
+    }
+}
