45f709a42266372951de484e98525c62a9e74d95
Robin Hanjun Kwon <khj02021@gmail.com>
2020-09-12 20:55:10 +0900

    [AR20] Implemented pressure plate and door mechanics
      - Implemented mechanics of pressure plate and door

---
+++ Assets/Scripts/Interactable/Door.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Sprite openDoorSprite;
+
+    private SpriteRenderer spriteRenderer;
+    private Sprite closedDoorSprite;
+    private BoxCollider2D doorCollider;
+    [HideInInspector] public bool isOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        doorCollider = GetComponent<BoxCollider2D>();
+        isOpen = false;
+        closedDoorSprite = spriteRenderer.sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isOpen) {
+            spriteRenderer.sprite = openDoorSprite;
+            doorCollider.enabled = false;
+        }
+        else {
+            spriteRenderer.sprite = closedDoorSprite;
+            doorCollider.enabled = true;
+        }
+    }
+}
