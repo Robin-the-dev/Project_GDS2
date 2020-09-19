270c7d6824b28ca235d49b04325d07e6ba61fe2d
Dawn Felstar <thelegotechhead@yahoo.com.au>
2020-09-20 09:49:09 +1000

    Gem changes
    Added Coloured lights to gems, Gems now have unique score values, Maki can now pick up gems, gems added to tutorial
    
    [AR23] As a player, I want the gems for score so that I can beat the high score [8] - 1.5 hour

--- Assets/Scripts/Collectables/Gem.cs
+++ Assets/Scripts/Collectables/Gem.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
-public class Gem : MonoBehaviour
-{
-    private float score;
+public class Gem : MonoBehaviour {
+    public float score = 10;
+    private Light2D light;
+    public Color color1;
+    public Color color2;
+    public float flickerDuration = 1f;
 
-    private void Start()
-    {
-        score = 100.0f;
+    void Start() {
+        light = GetComponentInChildren<Light2D>();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
+    void Update() {
+        float t = Mathf.PingPong(Time.time, flickerDuration) / flickerDuration;
+        light.color = Color.Lerp(color1, color2, t);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Pixie") {
             GameManager.instance.AddScore(score);
             Destroy(gameObject);
         }
