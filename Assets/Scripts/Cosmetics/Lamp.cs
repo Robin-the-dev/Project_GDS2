bd7f0f2615c0ef78fd10f9ddd9e92dcf57d30fae
Dawn Felstar <thelegotechhead@yahoo.com.au>
2020-10-23 12:30:37 +1100

    Added door sounds and totem sounds
    god i hope this works

--- Assets/Scripts/Cosmetics/Lamp.cs
+++ Assets/Scripts/Cosmetics/Lamp.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lamp : MonoBehaviour
-{
+public class Lamp : MonoBehaviour {
     [HideInInspector] public AudioSource lampAudioSource;
 
     // Start is called before the first frame update
--- Assets/Scripts/Interactable/DoorAnimated.cs
+++ Assets/Scripts/Interactable/DoorAnimated.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class DoorAnimated : LinkedObject {
     public bool isOpen;
     private Animator anim;
+    private AudioSource source;
     private SpriteRenderer sprite;
     private BoxCollider2D collider;
+
+    public bool isBig = false;
+
+    private AudioClip scrape;
+    private AudioClip bang;
+    private AudioClip open;
+
+    bool playSound = false;
+    float wait = 3;
 
     // Start is called before the first frame update
     void Awake() {
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         collider = GetComponent<BoxCollider2D>();
-        if(isOpen) openDoor();
+        source = GetComponent<AudioSource>();
+        scrape = Resources.Load<AudioClip>("Sounds/SoundEffects/Scrape Stone");
+        bang = Resources.Load<AudioClip>("Sounds/SoundEffects/Rumble");
+        open = Resources.Load<AudioClip>("Sounds/SoundEffects/Cave Level Opening");
+        if (isOpen) openDoor();
+    }
+
+    public void Update(){
+        if (wait > 0) {
+            wait-= Time.deltaTime;
+        } else {
+            playSound = true;
+        }
     }
 
     public override void Activate(string msg) {
@@ -24,15 +46,33 @@ public class DoorAnimated : LinkedObject {
         closeDoor();
     }
 
+    public void DoorOpened() {
+        if (isBig) {
+            source.volume = PlayerPrefs.GetFloat("SoundEffectVolume");
+            if (playSound) source.PlayOneShot(open);
+        }
+    }
+
+    public void DoorClosed() {
+        if (isBig) {
+            source.volume = PlayerPrefs.GetFloat("SoundEffectVolume");
+            if (playSound) source.PlayOneShot(bang);
+        }
+    }
+
     private void openDoor() {
         if (isOpen) return;
         isOpen = true;
         anim.Play("Open");
+        source.volume = PlayerPrefs.GetFloat("SoundEffectVolume");
+        if (playSound) source.PlayOneShot(scrape);
     }
 
     private void closeDoor() {
         if (!isOpen) return;
-        anim.Play("Close");
         isOpen = false;
+        anim.Play("Close");
+        source.volume = PlayerPrefs.GetFloat("SoundEffectVolume");
+        if (playSound) source.PlayOneShot(scrape);
     }
 }
